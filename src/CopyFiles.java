39396b563a667fb8a77e7412dc10061e8aca7a0d
Dell <juratergashev580@gmail.com>
2024-02-21 16:12:01 +0500

    add CopyFiles

---
+++ src/CopyFiles.java
@@ -0,0 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFiles {
+    public static void main(String[] args) {
+
+
+        copy("file 1", "file 2");
+    }
+
+    private static void copy(String originalPath, String copyPath) {
+        try {
+            FileInputStream inputStream = new FileInputStream(originalPath);
+            FileOutputStream outputStream = new FileOutputStream(copyPath);
+            int i = 0;
+            while ((i = inputStream.read()) != -1) {
+                outputStream.write(i);
+                outputStream.flush();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
